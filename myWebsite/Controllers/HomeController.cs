86135f87a879cb3b6182cf0516cb68638520bea6
Conor Halpin <chalpin994@gmail.com>
2017-04-05 16:13:42 +0100

    Renamed the solution
    Renamed the solution from "MovieDB" to "myWebsite" as I am building the
    website using the project I introduced myself to C# MVC with.
    
    I cleared out almost all of the MovieDB related code.
    
    I have embedded my LinkedIn profile on my index page.

---
+++ myWebsite/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using myWebsite.Models;
+
+namespace myWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+
+            return View();
+
+        }
+
+    }
+}
---
+++ myWebsite/Models/Model1.cs
@@ -0,0 +1,28 @@
+namespace myWebsite.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Table> Tables { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Table>()
+                .Property(e => e.Title)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Table>()
+                .Property(e => e.Director)
+                .IsFixedLength();
+        }
+    }
+}
