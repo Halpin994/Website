c8bc764d5774b6646a1ee79113e8d4cda8f07309
Conor Halpin <chalpin994@gmail.com>
2017-05-03 19:18:23 +0100

    Added correct unit test
    I added the wrong unit test, I added the visual studio unit test project
    and not the nunit one. Which is the one I wanted so I could run it
    through Jenkins.

--- MvcWebsite.Tests.NUnit/Properties/AssemblyInfo.cs
+++ MvcWebsite.Tests.NUnit/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcWebsite.Tests.Unit")]
+[assembly: AssemblyTitle("MvcWebsite.Tests.NUnit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("MvcWebsite.Tests.Unit")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nunit.tests")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("60e32bf7-4108-4ecb-bbed-30df98b37075")]
 
 // Version information for an assembly consists of the following four values:
 //
