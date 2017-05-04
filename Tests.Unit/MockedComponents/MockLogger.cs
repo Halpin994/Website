9ec68fc378e44b1f34a272fc7bed0e122bd53e45
Conor Halpin <chalpin994@gmail.com>
2017-05-04 18:39:24 +0100

    Added some mock components to begin testing the broker api
    Also recreated the test project as I had named it inappropriately
    before. I renamed it but this was not as easy as just renaming the
    project.

---
+++ Tests.Unit/MockedComponents/MockLogger.cs
@@ -0,0 +1,19 @@
+﻿using MvcWebsite.Logger;
+
+namespace Tests.Unit.MockedComponents
+{
+    public class MockLogger : ILogger
+    {
+        public void LogPageVisit(string input)
+        {
+        }
+
+        public void LogException(string input)
+        {
+        }
+
+        public void LogComment(string input)
+        {
+        }
+    }
+}
