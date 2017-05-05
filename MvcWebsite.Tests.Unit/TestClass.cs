5cd714d5b9d58c8f067f771e622b67f09dde476c
Conor Halpin <chalpin994@gmail.com>
2017-05-05 02:35:22 +0100

    Revert "Changes"
    This reverts commit 3347fba2b87ad4b26ad7eda0489d07f53306138f.

--- MvcWebsite.Tests.Unit/TestClass.cs
+++ MvcWebsite.Tests.Unit/TestClass.cs
@@ -18,5 +18,38 @@ namespace MvcWebsite.Tests.Unit
             result = 2 + 2;
             Assert.AreEqual(result, 4);
         }
+
+        [Test]
+        [TestCase("Index")]
+        public void MyTestMethod(String input)
+        {
+            var messageBrokerApi = new MessageBrokerApi(new MockLogger(), new MockHttpClientSimpleFactory());
+            var actualComments = messageBrokerApi.GetPageComments("Index").ToList();
+ 
+            var expectedComments = new List<CommentModel>()
+            {
+                new CommentModel()
+                {
+                    Comment = "testComment",
+                    Id = 1,
+                    UserName = "testUsername",
+                    Webpage = "Index"
+                }
+            };
+ 
+            Assert.AreEqual(actualComments.Count, expectedComments.Count);
+ 
+            foreach (var expected in expectedComments)
+            {
+                if (actualComments.Any(comment => comment.Id.Equals(expected.Id)))
+                {
+                    var actualComment = actualComments.First(comment => comment.Id.Equals(expected.Id));
+                    Assert.AreEqual(expected.Id,actualComment.Id);
+                    Assert.AreEqual(expected.Comment,actualComment.Comment);
+                    Assert.AreEqual(expected.UserName,actualComment.UserName);
+                    Assert.AreEqual(expected.Webpage,actualComment.Webpage);
+                }
+            }
+        }
     }
 }
