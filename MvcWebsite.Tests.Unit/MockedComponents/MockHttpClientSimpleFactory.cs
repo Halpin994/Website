3421c1f704356e75ce3b7d9362d0ebc5473dca5e
Conor Halpin <chalpin994@gmail.com>
2017-05-05 12:05:10 +0100

    Added the mock components back in

---
+++ MvcWebsite.Tests.Unit/MockedComponents/MockHttpClientSimpleFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using MvcWebsite.HttpClientFactory;
+
+
+namespace MvcWebsite.Tests.Unit.MockedComponents
+{
+    class MockHttpClientSimpleFactory : IHttpClientSimpleFactory
+    {
+        public HttpClient CreateClient()
+        {
+            var client = new HttpClient(new MockHttpMessageHandler())
+            {
+                BaseAddress = new Uri(new Uri("http://127.0.0.1"), "testApiPath"),
+
+            };
+            return client;
+        }
+    }
+}
---
+++ MvcWebsite.Tests.Unit/MockedComponents/MockHttpMessageHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MvcWebsite.Tests.Unit.MockedComponents
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(
+                    "[" +
+                    "{" +
+                    "'Id':'1'," +
+                    "'Comment':'testComment'," +
+                    "'Username':'testUsername'," +
+                    "'Webpage':'Index'," +
+                    "}," +
+                    "{" +
+                    "'Id':'2'," +
+                    "'Comment':'testComment2'," +
+                    "'Username':'testUsername2'," +
+                    "'Webpage':'Contact'," +
+                    "}" +
+                    "]"
+                    ,
+                    Encoding.ASCII,
+                    "application/json"),
+            };
+
+            return await Task.FromResult(responseMessage);
+        }
+    }
+}
