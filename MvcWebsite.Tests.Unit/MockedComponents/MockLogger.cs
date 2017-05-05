1a839f515c22d51cc884fe07de04856b57f3fba6
Conor Halpin <chalpin994@gmail.com>
2017-05-05 19:38:04 +0100

    Added the mock logger back in without using the ILogger

---
+++ MvcWebsite.Tests.Unit/MockedComponents/MockLogger.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MvcWebsite.Tests.Unit.MockedComponents
+{
+    public class MockLogger
+    {
+        public void LogPageVisit(string input)
+        {
+        }
+
+        public void LogException(string input)
+        {
+        }
+
+        public void LogComment(string input)
+        {
+        }
+    }
+}
