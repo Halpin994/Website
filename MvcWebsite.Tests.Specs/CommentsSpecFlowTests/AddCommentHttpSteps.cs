b2e27f276ac4cb0c4eaa3c1b99abcf02b977093a
Conor Halpin <chalpin994@gmail.com>
2017-05-19 16:48:11 +0100

    Changed how the test works to avoid inappropriate failures/passes
    A random number is now added to the Webpage string. This is used to find
    the newly submitted test comment without having to reset the message
    broker service to avoid finding a previously submitted test comment.
    Resulting in a pass if the current test did not submit a comment
    correctly.
    
    checking for the any comments in the 3rd step before asigning the var
    actualComment ensures that an exception is not thrown but an assertion
    fails and the test ends.

--- MvcWebsite.Tests.Specs/CommentsSpecFlowTests/AddCommentHttpSteps.cs
+++ MvcWebsite.Tests.Specs/CommentsSpecFlowTests/AddCommentHttpSteps.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -18,14 +19,17 @@ namespace MvcWebsite.Tests.Specs.CommentsSpecFlowTests
         private HttpClientSimpleFactory _httpClientFactory = new HttpClientSimpleFactory(_settings);
         private List<CommentModel> _actualComments;
         private CommentModel _expectedComment = new CommentModel(){Comment = "SpecTestComment", Id = 0, UserName = "SpecTestUser", Webpage = "SpecTest"};
-
+        private Random R = new Random();
+        private int rand;
         [Given(@"I post a comment to the comments controller")]
         public void GivenIPostACommentToTheCommentsController()
         {
             using (var wc = new WebClient())
             {
+                rand = R.Next(10000);
                 wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-                var htmlResult = wc.UploadString("http://localhost:8888/api/comments/", "Id=0&Webpage=SpecTest&UserName=SpecTestUser&Comment=SpecTestComment");
+                var comment = "Id=0&Webpage=SpecTest" + rand + "&UserName=SpecTestUser&Comment=SpecTestComment";
+                var htmlResult = wc.UploadString("http://localhost:8888/api/comments/", comment);
             }
         }
         
@@ -41,22 +45,22 @@ namespace MvcWebsite.Tests.Specs.CommentsSpecFlowTests
 
             _actualComments = result.ToList();
 
-            _expectedComment.Id = _actualComments.Last().Id;
+            foreach(var c in _actualComments)
+            {
+                if (c.Webpage == "SpecTest" + rand)
+                _expectedComment.Id = c.Id;
+                _expectedComment.Webpage = "SpecTest" + rand;
+            }
         }
         
         [Then(@"the comment I posted is available")]
         public void ThenTheCommentIPostedIsAvailable()
         {
-            foreach (var actual in _actualComments)
-            {
-                if (_actualComments.Any(comment => comment.Id.Equals(_expectedComment.Id)))
-                {
-                    var actualComment = _actualComments.First(comment => comment.Id.Equals(_expectedComment.Id));
-                    Assert.AreEqual(_expectedComment.Comment, actualComment.Comment);
-                    Assert.AreEqual(_expectedComment.UserName, actualComment.UserName);
-                    Assert.AreEqual(_expectedComment.Webpage, actualComment.Webpage);
-                }
-            }
+            Assert.AreEqual(true, _actualComments.Any(comment => comment.Id.Equals(_expectedComment.Id)));
+            var actualComment = _actualComments.First(comment => comment.Id.Equals(_expectedComment.Id));
+            Assert.AreEqual(_expectedComment.Comment, actualComment.Comment);
+            Assert.AreEqual(_expectedComment.UserName, actualComment.UserName);
+            Assert.AreEqual(_expectedComment.Webpage, actualComment.Webpage);
         }
     }
 }
